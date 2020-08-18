ebd143c06d8043ec1e60b7ce4ddd69ae0ead76cc
ChrisAchinga <achinga.chris@gmail.com>
2020-08-18 19:31:36 +0300

    lint

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 
-
 class Form extends Component {
-    render(){
-        return(
-            <div >
-
-            <form onSubmit ={this.props.getLocation}>
-                <p>
-                	<button >Get Weather</button>
-                </p>
-            </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.getLocation}>
+          <p>
+            <button>See Weather</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
 }
 export default Form;
